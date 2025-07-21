f1d43bacb2367f377f701e3e87a98b7914109711
Alexander <alexandertra25@gmail.com>
2025-07-21 11:23:43 -0300

    Conclusão do desafio

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone("Iphone17");
+
+        meuIphone.tocar();
+        meuIphone.ligar("119999999");
+        meuIphone.selecionarMusica("Selecionando Musica");
+        meuIphone.exibirPagina("https://Apple.com");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
